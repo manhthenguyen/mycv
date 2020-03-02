de16b5f48823d39c45bcf1eba2602c049b936a12
Nguyen The Manh <manhnguyentto@gmail.com>
2020-03-02 22:22:09 +0700

    Updates

---
+++ precache-manifest.a26e1e2ada70a0c82bf66155df45c8ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5018f8d87166d5238c44ae6b6667bac1",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "c776e95d81ae226bb831",
+    "url": "/mycv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6a68e415e63174a297c",
+    "url": "/mycv/static/js/2.90167dd2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mycv/static/js/2.90167dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c776e95d81ae226bb831",
+    "url": "/mycv/static/js/main.f56e283c.chunk.js"
+  },
+  {
+    "revision": "e5ca1b02989602e4ac72",
+    "url": "/mycv/static/js/runtime-main.eea9ac04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mycv/static/media/logo.5d5d9eef.svg"
+  }
+]);
